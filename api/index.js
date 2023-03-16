49745e277d908b2c0f6c5812680228f086c3a599
ThatAdamM <79579625+LarryTLlama@users.noreply.github.com>
2023-03-16 18:54:05 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-const axios = require("axios")
 export default async function handler(req, res) {
   let result = await fetch("https://web.peacefulvanilla.club/maps/tiles/players.json")
   return res.status(200).json(await result.json());
